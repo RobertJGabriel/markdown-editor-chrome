4a7feb354a15a3f2b947151fe25e697bd7e94111
Robert James Gabriel <robert_gabriel@outlook.com>
2017-07-30 16:40:03 +0100

    Markdown editor

--- app/scripts.babel/app.js
+++ app/scripts.babel/app.js
@@ -1,36 +1,49 @@
 'use strict';
 
-const blogJson = 'https://www.teamwork.com/index.cfm?action=whatsnew2';
-
 function sleep(time) {
-	const d1 = new Date().getTime();
-	let d2 = new Date().getTime();
-	while (d2 < d1 + time) {
-		d2 = new Date().getTime();
-	}
+
+  let d1 = new Date().getTime();
+  let d2 = new Date().getTime();
+  while (d2 < d1 + time) {
+    d2 = new Date().getTime();
+  }
 }
 
 new Vue({
-	el: '#app',
-	data: {
-		posts: [],
-		numberOfPosts: 0
-	},
-	beforeCompile: function beforeCompile() {
-		sleep(3000);
-	},
-	mounted() {
-		this.fetchPosts();
-	},
-	methods: {
-		fetchPosts() {
-			this.$http.get(blogJson).then(response => {
-				this.posts = response.body.results;
-				this.numberOfPosts = response.body.results.length;
-			}, response => {
-			});
-		}
-	}
+  el: '#app',
+  data: {
+    input:
+`
+
+# About
+Hodor, hodor... Hodor hodor hodor.
+
+# Set up
+Hodor, hodor... Hodor hodor hodor.
+
+# Install
+1. Hodor, hodor...
+2. Hodor, hodor...
+
+## Learn more
+[Markdown Cheat sheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+`
+  },
+  beforeCompile: function beforeCompile() {
+    sleep(3000);
+  },
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.input, {
+        sanitize: true
+      })
+    }
+  },
+  methods: {
+    update: _.debounce(function (e) {
+      this.input = e.target.value
+    }, 300)
+  }
 });
-Vue.use(VueMarkdown);
+
 Vue.config.productionTip = false;
