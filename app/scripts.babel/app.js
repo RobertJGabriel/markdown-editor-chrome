7152f1f7125d756c561887ec003e8857a6d120f7
Robert James Gabriel <robert_gabriel@outlook.com>
2018-02-03 16:27:37 +0000

    Added features

--- app/scripts.babel/app.js
+++ app/scripts.babel/app.js
@@ -6,10 +6,10 @@ Vue.config.productionTip = false;
 var markdownString =
   '### What is Markdown? \n \n' +
   ' Markdown is a lightweight markup language with plain text formatting syntax.  \n \n' +
-  '#### Extras \n \n' +
-  '- Fenced code blocks \n' +
+  '#### Version 2.0.0 \n \n' +
   '- Now auto saves your work!!! \n' +
   '- Tables \n' +
+  '- Fenced code blocks \n' +
   '- Even More \n\n' +
   '```javascript \n\n' +
   'function javascriptIsWild(){ \n \n' +
