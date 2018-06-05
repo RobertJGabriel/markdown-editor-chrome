28d546f07c1055bf0ad90daa5f77881ab1b5a42b
Robert James Gabriel <robert_gabriel@outlook.com>
2018-06-04 21:53:23 -0500

    Revert "meesage"
    This reverts commit 595756227aff3bc1615da42c0189d18127f0876f, reversing
    changes made to 7ccb1b4a672dde1e84c46273946f1b02f478c5c5.

--- app/scripts.babel/app.js
+++ app/scripts.babel/app.js
@@ -3,43 +3,77 @@
 const paid = false;
 Vue.config.productionTip = false;
 
+var markdownString =
+  '### What is Markdown? \n \n' +
+  ' Markdown is a lightweight markup language with plain text formatting syntax.  \n \n' +
+  '#### Features \n \n' +
+  '- Tables \n' +
+  '- Fenced code blocks \n' +
+  '- Even More \n\n' +
+  '```javascript \n\n' +
+  'function javascriptIsWild(){ \n \n' +
+  'parseInt("Infinity", 10) // -> NaN \n\n' +
+  '}\n' +
+  '```';
 
-var getSEOReportURL = 'https://seo-bot-.herokuapp.com/results';
-
-function sleep(time) {
-  var d1 = new Date().getTime();
-  var d2 = new Date().getTime();
-  while (d2 < d1 + time) {
-    d2 = new Date().getTime();
-  }
-  return;
+if (!paid) {
+  markdownString += '\n\n ### Premium Version \n\n 1. Auto save your work!! \n\n 2. Only 99 cent \n\n 3. Download your work to the desktop';
 }
 
+//If this is the paid version load 
+if (paid) {
+  getData();
+}
 
+// Get all saved data
+function getData() {
+  // Check if local storage is enabled
+  if (localStorage.getItem('storedData') !== null) {
+    // Load the data if needed
+    markdownString = localStorage.getItem('storedData');
+  }
+}
+
+// Save data to local storage
+function saveData(input) {
+  if (typeof Storage !== 'undefined') {
+    return localStorage.setItem('storedData', input)
+  }
+}
 
 new Vue({
   el: '#app',
   data: {
-    features: [],
-    css: [],
-    loading:null,
-    other: [],
-    input: 'https://www.robertgabriel.ninja'
+    paid: paid,
+    input: markdownString
   },
   watch: {
     input: function () {
       /* function to detect if localstorage is supported*/
-      return this.input;
+      if (paid) {
+        return saveData(this.input);
+      } else {
+        return this.input
+      }
+
     }
   },
 
   mounted: function () {
-
-
+    var code = this.input;
+    marked.setOptions({
+      highlight: function (code) {
+        return hljs.highlightAuto(code).value;
+      }
+    });
+    hljs.initHighlighting();
   },
   computed: {
     compiledMarkdown: function () {
-
+      return marked(this.input, {
+        langPrefix: 'hljs ',
+        xhtml: true
+      });
     }
   },
   methods: {
@@ -47,18 +81,15 @@ new Vue({
       this.input = e.target.value;
     }, 200),
     changeHandler: function () {
-      return this.input;
+      return marked(this.input);
     },
-    getSEOReport: function() {
-      this.loading = true;
-      this.$http.get(getSEOReportURL + '?name=' + encodeURI(this.input)).then(response => {
+    saveLocally: function () {
+      //  Escape HTML
+      var link = document.createElement('a');
+      link.download = 'README.md';
+      link.href ='data:text/plain,' + this.input;
 
-        console.log(response);
-        this.features = JSON.parse(response.bodyText).features; // Parse the coffee lists
-        this.css = this.features.css; // Parse the coffee lists
-        this.loading = false;
-      }, response => {
-      });
-  }
+      link.click(); // trigger click/download
+    }
   }
 });
